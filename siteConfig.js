976b9aa39289063a166d3f838c9bd313a0023bd8
Chris Hinds <chris@chrishinds.dev>
2020-09-08 21:28:57 +0100

    Update index title, add new favicon and update nav height on mobile

--- siteConfig.js
+++ siteConfig.js
@@ -2,8 +2,8 @@ module.exports = {
   name: "chrishinds.dev",
   shortName: "chrishinds.dev",
   description:
-    "A bold, minimal theme for Gatsby, focused on clean typography & beautiful imagery",
-  url: "https://chrishinds.dev", // Domain of your site without prefix!
+    "HI! I'm Chris, A software enginner with a passion for web performance currently making blazing fast web pages at the BBC.",
+  url: "https://www.chrishinds.dev", // Domain of your site without prefix!
   prefix: "/",
   author: "Chris Hinds",
   twitter: "chinds185a"
